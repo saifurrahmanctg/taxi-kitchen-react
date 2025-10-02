27ed83fa0b0b6f7b786033baa6884d2817986c10
saifurrahmanctg <chysaifurrahmanbd@gmail.com>
2025-10-02 22:01:47 +0600

    re commit

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,8 +4,7 @@ import React from "react";
 const Banner = () => {
   return (
     <h2
-      className={`bg-[url(./assets/title-bg.png)] flex justify-center items-center gap-1 text-4xl font-bold text-center bg-no-repeat pb-20 pt-10 bg-center`}
-      style={{ backgroundSize: "100% 100%" }}
+      className={`banner-bg flex justify-center items-center gap-1 text-4xl font-bold text-center pb-20 pt-10 `}
     >
       <ChefHat size={40} />
       Kitchen Room
